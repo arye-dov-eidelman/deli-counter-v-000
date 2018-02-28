c30b5fc99c7950139f11f233294a9eb7847351e8
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-28 17:11:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+require 'pry'
+def line(deli_line)
+  if deli_line.length == 0
+    return puts 'The line is currently empty.'
+  end
+  result = 'The line is currently: '
+  deli_line.each_with_index do |name, i|
+    binding.pry
+    result += "#{i+1.to_s}. #{name}"
+    result += ', ' if i+1 == deli_line.length
+  end
+  puts result
+end
